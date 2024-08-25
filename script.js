cc029d8e9599a19a1b51d92ef920aafcb08683d8
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-08-25 17:07:27 +0200

    Last Changes

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const formEl = document.querySelector("main aside form");
 
 formEl.addEventListener("submit", function (event) {
   event.preventDefault(); // prevent the default form submission
-  var title = document.getElementById("title").value;
-  var time = document.getElementById("time").value;
+
   const submitObj = {
     title: event.target.title.value,
     time: event.target.time.value,
     ingredients: event.target.ingredients.value,
   };
   event.target.reset(); //Reset the user input field
+
   //Function
   addRecipe(submitObj);
 });
